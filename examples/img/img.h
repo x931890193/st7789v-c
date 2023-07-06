22d92d4c1dbb29dc44b69af57cf5f8770761102c
sato <ysudqfs@163.com>
2023-07-06 23:53:51 +0800

    test

--- examples/img/img.h
+++ examples/img/img.h
@@ -19,14 +19,15 @@
 *******************************************************************************/
 
 
+ typedef struct {
+     const uint8_t *data;
+     uint16_t width;
+     uint16_t height;
+     uint8_t dataSize;
+     } tImage;
+
 #include <stdint.h>
 
-typedef struct {
-    const uint8_t *data;
-    uint16_t width;
-    uint16_t height;
-    uint8_t dataSize;
-} tImage;
 
 
 static const uint8_t image_data_wifi[2048] = {
